8a7262e59475ed0e47ec29687af1d7eeccba3ecc
PavelR <repening.pavel@gmail.com>
2019-11-12 18:16:09 +0400

    create shopping cart layout and simple style for it

---
+++ index.js
@@ -0,0 +1,8 @@
+let storage=[
+	{id: 0, name: "CPU", quantity: 3, price: 5000},
+	{id: 1, name: "GPU", quantity: 5, price: 7000},
+	{id: 2, name: "HDD", quantity: 4, price: 2000},
+	{id: 3, name: "SSD", quantity: 7, price: 3000},
+	{id: 4, name: "Monitor", quantity: 2, price: 8000},
+	{id: 5, name: "Printer", quantity: 3, price: 15000},
+]
