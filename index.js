1002ffe0ab64a377836e7a201e721287e2f23cb5
MaryFeles <trubnikova.mp@gmail.com>
2019-11-14 13:52:28 +0400

    .

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let storage=[
 	{id: 6, name: "Printer", quantity: 3, price: 15000},
 ]
 
+let bascket = [];
+
 const itemList = document.getElementById("item-list");
 const cartList = document.getElementById("cart-list");
 const itemRows = itemList.getElementsByTagName("tbody")[0];
@@ -14,6 +16,8 @@ const cartRows = cartList.getElementsByTagName("tbody")[0];
 const itemColumns = itemList.getElementsByTagName("td");
 const cartColumns = cartList.getElementsByTagName("td");
 const price = document.getElementById("price");
+
+
 
 
 let columnsId = [];
@@ -29,7 +33,13 @@ function main(){
 	}
 }
 
-main();
+
+
+//main();
+
+document.addEventListener("DOMContentLoaded", main);
+
+
 
 function addRow(tableRows, item){
 	let row = tableRows.insertRow(tableRows.rows.length);
@@ -40,22 +50,91 @@ function addRow(tableRows, item){
 		cell.className += key;
 		row.setAttribute( 'data-id', tableRows.rows.length);
 	}
-	/*console.log(row);*/
-}
-
-/*console.log(itemRows.rows.length);*/
-
-
-
-
-function clickListener(){
 	for(let i = 0; i < itemRows.rows.length; i++){
 		itemRows.rows[i].addEventListener('click', addToCart)
+
 	}
+	console.log();
 }
 
-function addToCart(){
+
+function renderBasket(){
+	let f=false;
+
+	console.log(bascket);
+
+	let cache;
 	
+	for (let i = 0; i < bascket.length; i++){
+		cache = bascket[bascket.length-1]["name"];
+		for (let j = 0; j < bascket.length-1; j++){
+			if(cache == bascket[j]["name"]){
+			 // bascket[bascket.length-1]["quantity"]++;
+			  f = false;	
+			 //productPrice+=productPrice;
+			  break;		 
+			} else f = true;
+		} 
+	}
+	if ((f) || (bascket.length == 1)) {addRowCart(cartRows, bascket[bascket.length-1]);
+	}	
 }
 
-clickEvent();
+function addRowCart(tableRows, item){
+	row = tableRows.insertRow();	
+	for (let i = 0; i < columnsId.length; i++){
+		let cell = row.insertCell(i);
+		let key = columnsId[i];
+		cell.className += key;
+
+		//cell.innerHTML = item[key];
+
+		if (cell.getAttribute('class') == "name"){
+			cell.innerHTML = item["name"];
+		}
+
+		/*if (cell.getAttribute('class') == "price"){
+			cell.innerHTML = item["price"];
+		}*/
+
+		console.log();
+		//if (cell.getAttribute('class') == "quantity"){
+		//	cell.innerHTML = 1;
+		//}
+	}	
+}
+
+
+function addToCart(e) {
+	let dataId = e.target.parentNode.getAttribute('data-id');
+	let foundStoreItem = storage.find(function(currentElem) {
+		if (currentElem.id == dataId) {
+			return true;
+		}
+		return false;
+
+	});
+	if (foundStoreItem.quantity == 0) {
+		alert('Failed');
+	} else {
+		foundStoreItem.quantity--;
+		bascket.push(foundStoreItem);
+		renderBasket();
+		//console.log(foundStoreItem.id);
+		
+	}
+	//console.log(foundStoreItem);
+
+	//bascket.push()
+	/*let row = cartRows.insertRow(cartRows.rows.length);
+	if (+(this.children[2].textContent) > 0){
+		for (let i = 0; i < cartColumns.length; i++){
+			let cell = row.insertCell(i);
+			console.log(this.children[i].textContent);
+			cell.innerHTML = this.children[i].textContent;
+
+			this.children[2].innerHTML = +(this.children[2].textContent) - 1;
+	}
+} else alert("error");*/
+//	console.log(this.children[1].textContent);
+}
